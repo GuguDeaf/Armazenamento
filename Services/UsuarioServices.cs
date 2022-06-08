77841ad7c227d46af0d4e2a827f6083184ed8f00
Nélio Augusto <106417497+GuguDeaf@users.noreply.github.com>
2022-06-08 19:03:45 -0300

    Adicionado filmes

---
+++ Services/UsuarioServices.cs
@@ -0,0 +1,40 @@
+﻿using AulasPCDev.Models;
+using AulasPCDev.Respository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AulasPCDev.Services
+{
+    public class UsuarioServices
+    {
+        private static List<Usuario> _listaUsuarios = Armazenamento.Usuarios;
+
+        public void ListarUsuarios()
+        {
+            Console.WriteLine("NOME - IDADE - SALDO");
+            foreach (Usuario usuario in _listaUsuarios)
+            {
+                Console.WriteLine(usuario.Nome + " - " +
+                    usuario.Idade + " - " +
+                    usuario.Conta.Saldo);
+            }
+        }
+
+        public void CadastrarUsuario()
+        {
+            Console.WriteLine("Digite o nome do usuário:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a idade do usuário:");
+            int idade = int.Parse(Console.ReadLine());
+
+            Usuario usuario = new Usuario(idade);
+            usuario.Nome = nome;
+            Console.WriteLine("Idade: " + usuario.Idade);
+            Console.WriteLine("Bem vindo a locadora " + usuario.Nome + ", por ser novo aqui você recebeu um crédito de " + usuario.Conta.ToString() + ".");
+
+            _listaUsuarios.Add(usuario);
+        }
+    }
+}
